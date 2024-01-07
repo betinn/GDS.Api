c7f652b0a1c9d5065a7a3d531d9464d47e863b3b
betinn <86447087+betinn@users.noreply.github.com>
2024-01-07 19:02:16 -0300

    Authorize attribute Cardcontroller

--- src/GDS.Api/Controllers/CardController.cs
+++ src/GDS.Api/Controllers/CardController.cs
@@ -1,6 +1,7 @@
 ﻿using GDS.Api.Filter;
 using GDS.Api.Model.Request;
 using GDS.Api.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GDS.Api.Controllers
@@ -10,16 +11,19 @@ namespace GDS.Api.Controllers
     public class CardController(ICardService cardService) : Controller
     {
         private readonly ICardService _cardService = cardService;
+        [Authorize]
         [HttpPost("create")]
         public IActionResult Create([FromBody] CreateCardRequest cardRequest)
         {
             return Ok(_cardService.Create(cardRequest));
         }
+        [Authorize]
         [HttpPut("update/{idCard}")]
         public IActionResult Update(Guid idCard, [FromBody] UpdateCardRequest updateCardRequest)
         {
             return Ok(_cardService.Update(idCard, updateCardRequest));
         }
+        [Authorize] 
         [HttpDelete("delete/{idCard}")]
         public IActionResult Delete(Guid idCard)
         {
